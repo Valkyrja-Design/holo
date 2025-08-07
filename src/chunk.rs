a8437f447cf6732263a61f7a13bc8ca5729c425c
Manish Kumawat <manish6000.mk@gmail.com>
2025-08-07 12:12:59 +0000

    Add few basic opcodes, and a disassembler

---
+++ src/chunk.rs
@@ -0,0 +1,122 @@
+use super::value;
+
+#[derive(Clone, Copy)]
+pub enum OpCode {
+    OpConstant,
+    OpConstantLong,
+    OpReturn,
+}
+
+impl From<u8> for OpCode {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => OpCode::OpConstant,
+            1 => OpCode::OpConstantLong,
+            2 => OpCode::OpReturn,
+            _ => panic!("invalid opcode!"),
+        }
+    }
+}
+
+impl From<OpCode> for u8 {
+    fn from(value: OpCode) -> u8 {
+        match value {
+            OpCode::OpConstant => 0,
+            OpCode::OpConstantLong => 1,
+            OpCode::OpReturn => 2,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct LineInfo {
+    byte_idx: usize,
+    line: usize,
+}
+
+#[derive(Debug)]
+pub struct Chunk {
+    pub code: Vec<u8>,
+    pub constants: Vec<value::Value>,
+    pub line_info: Vec<LineInfo>,
+}
+
+impl Chunk {
+    pub fn new() -> Self {
+        Chunk {
+            code: vec![],
+            constants: vec![],
+            line_info: vec![],
+        }
+    }
+
+    pub fn write_byte(&mut self, byte: u8, line: usize) {
+        self.code.push(byte);
+
+        if let Some(LineInfo {
+            byte_idx: _,
+            line: prev_line,
+        }) = self.line_info.last()
+        {
+            if *prev_line != line {
+                self.line_info.push(LineInfo {
+                    byte_idx: self.code.len() - 1,
+                    line,
+                });
+            }
+        } else {
+            self.line_info.push(LineInfo { byte_idx: 0, line });
+        }
+    }
+
+    pub fn write_bytes(&mut self, bytes: &[u8], lines: &[usize]) {
+        for (byte, line) in bytes.iter().zip(lines.iter()) {
+            self.write_byte(*byte, *line);
+        }
+    }
+
+    pub fn write_opcode(&mut self, opcode: OpCode, line: usize) {
+        self.write_byte(opcode.into(), line);
+    }
+
+    pub fn write_as_24bit_int(&mut self, mut value: usize, line: usize) {
+        const MASK_BYTE: usize = (1usize << 8) - 1;
+        let mut bytes: [u8; 3] = [0; 3];
+
+        bytes[2] = (value & MASK_BYTE) as u8;
+        value >>= 8;
+
+        bytes[1] = (value & MASK_BYTE) as u8;
+        value >>= 8;
+
+        bytes[0] = (value & MASK_BYTE) as u8;
+
+        self.write_bytes(&bytes, &[line; 3]);
+    }
+
+    pub fn add_constant(&mut self, value: value::Value) -> usize {
+        self.constants.push(value);
+
+        self.constants.len() - 1
+    }
+
+    pub fn read_as_24bit_int(bytes: &[u8]) -> usize {
+        let a = bytes[0] as usize;
+        let b = bytes[1] as usize;
+        let c = bytes[2] as usize;
+
+        (a << 16) + (b << 8) + c
+    }
+
+    pub fn get_line_of(&self, byte_idx: usize) -> usize {
+        let high = self.line_info.partition_point(|x| x.byte_idx <= byte_idx);
+
+        self.line_info[high - 1].line
+    }
+}
+
+impl Default for Chunk {
+    fn default() -> Self {
+        Chunk::new()
+    }
+}
---
+++ src/disassembler.rs
@@ -0,0 +1,91 @@
+use super::chunk;
+
+pub struct Diassembler<'a> {
+    chunk: &'a chunk::Chunk,
+    chunk_name: &'a str,
+}
+
+impl<'a> Diassembler<'a> {
+    fn new(chunk: &'a chunk::Chunk, chunk_name: &'a str) -> Diassembler<'a> {
+        Diassembler { chunk, chunk_name }
+    }
+
+    fn disassemble(&self) {
+        println!("== {} ==", self.chunk_name);
+
+        let mut offset: usize = 0;
+
+        while offset < self.chunk.code.len() {
+            offset = self.disassemble_instr(offset);
+        }
+    }
+
+    fn disassemble_instr(&self, offset: usize) -> usize {
+        print!("{:04} {:04} ", offset, self.chunk.get_line_of(offset));
+
+        let instr = self.chunk.code[offset];
+
+        match chunk::OpCode::from(instr) {
+            chunk::OpCode::OpConstant => self.const_instr(offset),
+            chunk::OpCode::OpConstantLong => self.const_long_instr(offset),
+            chunk::OpCode::OpReturn => self.simple_instr("OP_RETURN", offset),
+            _ => {
+                println!("Unknown opcode {}", instr);
+                offset + 1
+            }
+        }
+    }
+
+    fn const_instr(&self, offset: usize) -> usize {
+        let idx = self.chunk.code[offset + 1];
+
+        println!("OP_CONSTANT {}", self.chunk.constants[idx as usize]);
+
+        offset + 2
+    }
+
+    fn const_long_instr(&self, offset: usize) -> usize {
+        let idx = chunk::Chunk::read_as_24bit_int(&self.chunk.code[offset + 1..]);
+
+        println!("OP_CONSTANT_LONG {}", self.chunk.constants[idx]);
+
+        offset + 4
+    }
+
+    fn simple_instr(&self, name: &str, offset: usize) -> usize {
+        println!("{}", name);
+
+        offset + 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_chunk() {
+        let mut chunk = chunk::Chunk::default();
+
+        for _ in 0..256 {
+            let idx = chunk.add_constant(1.23) as u8;
+
+            chunk.write_opcode(chunk::OpCode::OpConstant, 1);
+            chunk.write_byte(idx, 1);
+        }
+
+        for _ in 256..512 {
+            let idx = chunk.add_constant(125.25);
+
+            chunk.write_opcode(chunk::OpCode::OpConstantLong, 2);
+            chunk.write_as_24bit_int(idx, 2);
+        }
+
+        chunk.write_opcode(chunk::OpCode::OpReturn, 3);
+        chunk.write_opcode(chunk::OpCode::OpReturn, 4);
+
+        let disassembler = Diassembler::new(&chunk, "simple test chunk");
+
+        disassembler.disassemble();
+    }
+}
